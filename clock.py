523cd0d56a00b4a110c47841a7559485a1d3c6e6
Brendan Horan <brendan@horan.hk>
2020-05-15 00:02:11 +0800

    fix clss logic

--- clock.py
+++ clock.py
@@ -28,11 +28,31 @@ class Esp32Rtc:
         self.rtc = machine.RTC()
 
     def set_rtc(self,date_time):
-        self.rtc.datetime(date_time)
+        time_data = requests.get('http://worldtimeapi.org/api/ip')
+        date_time = json.loads(time_data.text)
+    
+        #  value returned looks like : 2020-05-13T21:44:36.732570+08:00
+        date_time = (date_time['datetime'])
+        year = date_time[0:4]
+        month = date_time[5:7]
+        day = date_time[8:10]
+        hour = date_time[11:13]
+        minute = date_time[14:16]
+        second = date_time[17:19]
+        tz = date_time[26:29]
+
+        self.rtc.init((year, month, day, hour, minute, second, tz))
 
     def get_rtc(self):
         rtc_date_time = self.rtc.datetime()
-        return rtc_date_time
+        year = rtc_date_time[0]
+        month = rtc_date_time[1]
+        day = rtc_date_time[2]
+        hour = rtc_date_time[3]
+        minute = rtc_date_time[4]
+        second = rtc_date_time[5]
+
+        return year+month+day+hour+minute+second
 
 class B7Settings:
     def __init__(self):
@@ -71,10 +91,12 @@ def b7_message(serial_device,message):
     write_b7_message.write_data(_command_prefix+message.upper())
 
 def b7_blank_tube(serial_device, tube_id):
+   ''' not done yet '''
+   _command_prefix = '$B7M'
    blank_tube = SerialWrite(serial_device)
 
    # Space char(ASCII 32) will blank a tube
-   blank_tube.write_data(' ')
+   blank_tube.write_data(_command_prefix+' ')
 
 def b7_underscore(serial_device,message):
     _command_prefix ='$B7U'
@@ -92,28 +114,10 @@ def get_time_data_data(device):
         return date_time
     
     elif device == 'esp32':
-        time_data = requests.get('http://worldtimeapi.org/api/ip')
-        date_time = json.loads(time_data.text)
-    
-        #  value returned looks like : 2020-05-13T21:44:36.732570+08:00
-        date_time = (date_time['datetime'])
-        
-        # extract just the date 
-        date = date_time[0:10]
-        # strip the - from the date
-        date = date.replaces('-', '')
-        # extract just the time
-        current_time =  date_time[11:19]
-        # strip the : from the time
-        current_time = current_time.replaces(':', '')
-
-        return date + current_time
-
+        date_time = Esp32Rtc.get_rtc()
     else:
         # make this nicer, exception etc...
         print('unknown device')
-
-
 
 def format_time(date_time):
     date_time = date_time[8::]
@@ -127,22 +131,24 @@ def hours_24_to_12():
     pass
 
 
-class MainLoop():
-    ''' Main function where the clock spends its time, doing clock things '''
-    def __init__(self,device_type, socket_count, serial_device):
+class MainSetup:
+    def __init__(self, device_type, socket_count, serial_device):
         self.device_type = device_type
         self.serial_device = serial_device
+        self.socket_count = socket_count
 
 
+class MainLoop(MainSetup):
     def main(self):
         while True:
             get_time = get_time_data_data(self.device_type)
             current_time = format_time(get_time)
+            print(current_time)
             if current_time[4::] == '10':
                 self.read_temp_sensor()
             elif current_time[2::] == '1500':
                 self.cathode_poisoning_prevention(6)
-            elif current_time[2::] == '4100':
+            elif current_time[2::] == '4800':
                 self.display_date()
             else:
                 b7_message(self.serial_device, current_time)
@@ -156,8 +162,6 @@ class MainLoop():
     def display_date(self):
         raw_date = get_time_data_data(self.device_type)
         date = format_date(raw_date)
-        print('date to print : ')
-        print(date)
         time.sleep(1)
         b7_message(self.serial_device, date)
         time.sleep(1)
@@ -169,6 +173,7 @@ class MainLoop():
         try:
             # Effect type 8, spin full cycle
             b7_effect('/dev/ttyUSB0', '8' * self.socket_count)
+            # cycle digits from 0 to 10
             for number in range(10):
                 number = str(number)
                 number = number * self.socket_count
@@ -184,5 +189,6 @@ class MainLoop():
 
 if __name__ == "__main__":
 
-    MainLoop.main('pc', '6', '/dev/ttyUSB0')
-
+    run = MainLoop('pc', '6', '/dev/ttyUSB0')
+    run.main()
+    
