ff069d8ec75ac0997766affec641cb3c90307775
Brendan Horan <brendan@horan.hk>
2020-05-16 18:33:22 +0800

    fix pep issues in tests

--- tests/test_clock.py
+++ tests/test_clock.py
@@ -4,14 +4,13 @@ from mock import MagicMock
 from unittest.mock import patch
 
 
-
 date_time_test_value = '20200516133010'
 
 # def test_get_time_date_data(mocker):
 #    mocker.patch('clock.SerialWrite.write_data')
 #    mocker.patch('clock.datetime.datetime')
-#    # with patch('clock.datetime.datetime') as brr:
-#        # brr.return_value = '232'
+#     with patch('clock.datetime.datetime') as brr:
+#         brr.return_value = '232'
 #    assert clock.get_time_date_data('pc') == date_time_test_value
 
 
